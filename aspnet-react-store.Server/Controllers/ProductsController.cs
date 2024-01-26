d0e7f96ce74e651a580e0dac2477450d2d93cf7b
st-georgy <zhidkov.georg@gmail.com>
2024-01-26 17:33:18 +0300

    Simple get products controller

---
+++ aspnet-react-store.Server/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+using aspnet_react_store.Server.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace aspnet_react_store.Server.Controllers {
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController(StoreDbContext context) : ControllerBase {
+        private readonly StoreDbContext _context = context;
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts([FromQuery] int? startId, [FromQuery] int? count) {
+            if (!startId.HasValue || !count.HasValue) {
+                var allProducts = await _context.Products.ToListAsync();
+                return Ok(allProducts);
+            }
+
+            if (startId < 1 || count < 1)
+                return BadRequest("Invalid arguments (startId or count)");
+
+            var products = await _context.Products
+                                .Where(p => p.Id >= startId)
+                                .Take(count.Value)
+                                .ToListAsync();
+
+            return Ok(products);
+        }
+    }
+}
