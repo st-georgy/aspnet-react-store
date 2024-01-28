009b0f6945751c68373ad5b2c6cb8f1aa8d57b50
st-georgy <zhidkov.georg@gmail.com>
2024-01-28 20:58:10 +0300

    all models created

--- aspnet-react-store.client/src/models/IProduct.tsx
+++
@@ -1,7 +0,0 @@
-export default interface IProduct {
-    id: number;
-    name: string;
-    price: number;
-    description: string;
-    images: string[]
-}
---
+++ aspnet-react-store.client/src/models/models.tsx
@@ -0,0 +1,53 @@
+export interface IProduct {
+    id: number;
+    name: string;
+    price: number;
+    description?: string | null;
+}
+
+export interface ICart {
+    id: number;
+    user: IUser;
+    products: IProduct[];
+}
+
+export interface IOrder {
+    id: number;
+    user: IUser;
+    status: OrderStatus;
+    products: IProduct[];
+}
+
+export interface IUser {
+    id: number;
+    username: string;
+    cart: ICart;
+    info: IUserInfo;
+    accountType: AccountType;
+    orders: IOrder[];
+}
+
+export interface IUserInfo {
+    id: number;
+    user: IUser;
+    email?: string | null;
+    firstName?: string | null;
+    middleName?: string | null;
+    lastName?: string | null;
+    joinDate: Date;
+}
+
+export enum OrderStatus {
+    Pending,
+    Cancelled,
+    Declined,
+    Confirmed,
+    Shipped,
+    Completed,
+}
+
+export enum AccountType {
+    User,
+    Support,
+    Admin,
+}
